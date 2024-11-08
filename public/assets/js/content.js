668ce41d75fb8025aeeb1f95daad4984a5061524
不知火 Shiranui <supersonic@livemail.tw>
2024-11-08 18:29:30 +0800

    refactor: using content.js

---
+++ public/assets/js/content.js
@@ -0,0 +1,33 @@
+(async () => {
+    const { pathname } = window.location;
+    const dataPath = (filename) => `./assets/data/${filename}.json`;
+    const dataFetch = (filename) => fetch(dataPath(filename)).
+        then(response => response.json()).
+        catch(error => console.error(error));
+
+    const context = new Map();
+
+    const year = new Date().getFullYear();
+    context.set("year", year);
+
+    switch (pathname) {
+        case '/': {
+            const social = await dataFetch("social");
+            context.set("socialLinks", social);
+            break;
+        }
+        case '/gallery': {
+            const projects = await dataFetch("projects");
+            context.set("projects", projects);
+            break;
+        }
+    };
+
+    document.addEventListener('alpine:init', () => {
+        for (const [key, value] of context) {
+            console.log(`Setting ${key} to`, value);
+            Alpine.store(key, value);
+        }
+    });
+    Alpine.start();
+})();
